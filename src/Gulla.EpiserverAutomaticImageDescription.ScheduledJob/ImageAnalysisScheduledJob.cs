e24d66fe321dbe52fb299bebe8a591b1dfb6a765
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2019-12-12 21:13:41 +0100

    Add scheduled job template.

---
+++ src/Gulla.EpiserverAutomaticImageDescription.ScheduledJob/ImageAnalysisScheduledJob.cs
@@ -0,0 +1,36 @@
+﻿using EPiServer.PlugIn;
+using EPiServer.Scheduler;
+
+namespace Gulla.EpiserverAutomaticImageDescription.ScheduledJob
+{
+    [ScheduledPlugIn(DisplayName = "Analyze all images, and update metadata")]
+    public class ImageAnalysisScheduledJob : ScheduledJobBase
+    {
+        private bool _stopSignaled;
+
+        public ImageAnalysisScheduledJob()
+        {
+            IsStoppable = true;
+        }
+
+        public override string Execute()
+        {
+            OnStatusChanged("Analyzing images...");
+            OnStatusChanged("Nah, fuck it!");
+            OnStatusChanged("I quit!");
+            OnStatusChanged("Finished!");
+
+            if (_stopSignaled)
+            {
+                return "Job stopped!";
+            }
+
+            return "Job completed. No changes were made!";
+        }
+
+        public override void Stop()
+        {
+            _stopSignaled = true;
+        }
+    }
+}
