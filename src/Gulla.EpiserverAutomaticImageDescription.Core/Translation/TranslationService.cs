fde4b5e7d72b7571d72db6b0d646d2cf3c0e456c
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2020-09-22 17:02:50 +0200

    Stop using bearer token for translation api authentication

--- src/Gulla.EpiserverAutomaticImageDescription.Core/Translation/TranslationService.cs
+++ src/Gulla.EpiserverAutomaticImageDescription.Core/Translation/TranslationService.cs
@@ -5,7 +5,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Configuration;
-using Gulla.Episerver.AutomaticImageDescription.Core.Translation.Auth;
 using Gulla.Episerver.AutomaticImageDescription.Core.Translation.Cache;
 using Gulla.Episerver.AutomaticImageDescription.Core.Translation.DTO;
 using Newtonsoft.Json;
@@ -17,13 +16,10 @@ namespace Gulla.Episerver.AutomaticImageDescription.Core.Translation
         private static readonly string TranslatorSubscriptionKey = WebConfigurationManager.AppSettings["Gulla.Episerver.AutomaticImageDescription:Translator.SubscriptionKey"];
         private static readonly string TranslatorTokenServiceEndpoint = WebConfigurationManager.AppSettings["Gulla.Episerver.AutomaticImageDescription:Translator.TokenService.Endpoint"];
         private const string TranslatorEndpoint = "https://api.cognitive.microsofttranslator.com";
-        private readonly string _requestToken;
         private readonly TranslationCache _cache;
 
         private TranslationService()
         {
-            var auth = new AuthToken(TranslatorTokenServiceEndpoint, TranslatorSubscriptionKey);
-            _requestToken = Task.Run(() => auth.GetAccessTokenAsync()).Result;
             _cache = new TranslationCache();
         }
 
@@ -36,8 +32,7 @@ namespace Gulla.Episerver.AutomaticImageDescription.Core.Translation
 
             return new TranslationService();
         }
-
-
+        
         public IEnumerable<string> TranslateText(IEnumerable<string> inputText, string toLanguage, string fromLanguage)
         {
             var originalText = inputText.ToArray();
@@ -71,11 +66,9 @@ namespace Gulla.Episerver.AutomaticImageDescription.Core.Translation
             using (var request = new HttpRequestMessage())
             {
                 request.Method = HttpMethod.Post;
-
                 request.RequestUri = new Uri(TranslatorEndpoint + route);
                 request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
                 request.Headers.Add("Ocp-Apim-Subscription-Key", TranslatorSubscriptionKey);
-                request.Headers.Add("Authorization", _requestToken);
 
                 var response = await client.SendAsync(request).ConfigureAwait(false);
                 var result = await response.Content.ReadAsStringAsync();
