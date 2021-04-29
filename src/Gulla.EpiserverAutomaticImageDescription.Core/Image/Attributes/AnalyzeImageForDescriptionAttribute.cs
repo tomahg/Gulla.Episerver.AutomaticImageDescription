acf4f4e7a438344cd24e2ba7d300f8a5013704d2
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2021-04-29 14:47:12 +0200

    Update description attribute with suport for localized string

--- src/Gulla.EpiserverAutomaticImageDescription.Core/Image/Attributes/AnalyzeImageForDescriptionAttribute.cs
+++ src/Gulla.EpiserverAutomaticImageDescription.Core/Image/Attributes/AnalyzeImageForDescriptionAttribute.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gulla.Episerver.AutomaticImageDescription.Core.PropertyDefinitions;
 using Gulla.Episerver.AutomaticImageDescription.Core.Translation;
 using Gulla.Episerver.AutomaticImageDescription.Core.Translation.Constants;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
@@ -6,7 +9,7 @@ using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 namespace Gulla.Episerver.AutomaticImageDescription.Core.Image.Attributes
 {
     /// <summary>
-    /// Analyze image and create a description. Apply to string properties.
+    /// Analyze image and create a description. Apply to string or localized string properties.
     /// </summary>
     public class AnalyzeImageForDescriptionAttribute : BaseImageDetailsAttribute
     {
@@ -15,7 +18,7 @@ namespace Gulla.Episerver.AutomaticImageDescription.Core.Image.Attributes
         private readonly bool _endWithDot;
 
         /// <summary>
-        /// Analyze image and create a description. Apply to string properties.
+        /// Analyze image and create a description. Apply to string or localized string properties.
         /// </summary>
         /// <param name="languageCode">Translate description to specified language.</param>
         /// <param name="upperCaseFirstLetter">Uppercase first letter.</param>
@@ -38,21 +41,44 @@ namespace Gulla.Episerver.AutomaticImageDescription.Core.Image.Attributes
                 return;
             }
 
+            var description = imageAnalyzerResult.Description.Captions.Select(caption => caption.Text).FirstOrDefault();
+
             if (IsStringProperty(propertyAccess.Property))
             {
-                var descriptionTranslated = GetTranslatedDescription(imageAnalyzerResult.Description.Captions.Select(caption => caption.Text).FirstOrDefault(), translationService);
-                propertyAccess.SetValue(descriptionTranslated);
+                propertyAccess.SetValue(GetTranslatedDescription(description, translationService));
+            }
+            else if (IsLocalizedStringProperty(propertyAccess.Property))
+            {
+                propertyAccess.SetValue(GetTranslatedLocalizedStrings(description, GetLanguageCodes(), translationService));
             }
         }
 
         private string GetTranslatedDescription(string description, TranslationService translationService)
         {
-            if (_languageCode != null && !string.IsNullOrEmpty(description))
+            if (!string.IsNullOrEmpty(_languageCode) && _languageCode != TranslationLanguage.English && !string.IsNullOrEmpty(description))
             {
                 description = translationService.TranslateText(new[] { description }, _languageCode, TranslationLanguage.English).First();
             }
 
             return FormatDescription(description, _upperCaseFirstLetter, _endWithDot);
+        }
+
+        private IEnumerable<LocalizedString> GetTranslatedLocalizedStrings(string description, IEnumerable<string> languageCodes, TranslationService translationService)
+        {
+            return languageCodes.Select(languageCode => GetTranslatedLocalizedString(description, languageCode, translationService)).ToList();
+        }
+
+        private LocalizedString GetTranslatedLocalizedString(string description, string languageCode, TranslationService translationService)
+        {
+            if (languageCode == TranslationLanguage.English)
+            {
+                var formattedDescription = FormatDescription(description, _upperCaseFirstLetter, _endWithDot);
+                return new LocalizedString { Language = TranslationLanguage.English, Value = formattedDescription };
+            }
+
+            var translatedDescription = translationService.TranslateText(new[] { description }, languageCode, TranslationLanguage.English).First();
+            var formattedTranslatedDescription = FormatDescription(translatedDescription, _upperCaseFirstLetter, _endWithDot);
+            return new LocalizedString { Language = languageCode, Value = formattedTranslatedDescription };
         }
 
         private static string FormatDescription(string description, bool upperCaseFirstLetter, bool endWithDot)
@@ -68,6 +94,17 @@ namespace Gulla.Episerver.AutomaticImageDescription.Core.Image.Attributes
             }
 
             return description + (endWithDot && !description.Trim().EndsWith(".") ? "." : "");
+        }
+
+        private IEnumerable<string> GetLanguageCodes()
+        {
+            var languageCodes = _languageCode?.Split(new[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            if (_languageCode == TranslationLanguage.AllActive || languageCodes?.Any() != true)
+            {
+                languageCodes = new LanguageSelectionFactory().GetSelections(null).Select(x => x.Value as string).ToList();
+            }
+
+            return languageCodes;
         }
     }
 }
